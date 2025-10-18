97f7d3221d2b09528a854e643d807a6ef7400317
dimeBrii <thiswasbrii@gmail.com>
2025-10-18 11:36:51 -0400

    feat : adding react project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+function App() {
+	return (
+		<div>
+			<h1>Mi primer proyecto en react</h1>
+			<p>Hecho por bryant</p>
+			<img src={reactLogo} alt="" />
+		</div>
+	);
+}
+
+export default App;
